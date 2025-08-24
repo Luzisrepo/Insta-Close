5cbce0f8414a486b75fc5e5dbe4f25a6b1c78f27
._rayzz / luzisrepo <andrecunhamartins.w122@gmail.com>
2025-08-24 03:49:03 +0100

    Update close.java

--- close.java
+++ close.java
@@ -20,18 +20,18 @@ public class InstaClose {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        // Register our client-side event handler
+        
         MinecraftForge.EVENT_BUS.register(new GuiBlocker());
     }
 
-    /** Event handler that cancels any container GUI opening. */
+    
     public static class GuiBlocker {
         @SubscribeEvent
         public void onGuiOpen(GuiOpenEvent event) {
             GuiScreen gui = event.gui;
             if (gui == null) return;
 
-            // Any inventory/container-based screen extends GuiContainer (includes player inv, chests, furnaces, crafting, hopper, anvil, creative inv, etc.)
+            
             if (gui instanceof GuiContainer) {
                 event.setCanceled(true);
             }
